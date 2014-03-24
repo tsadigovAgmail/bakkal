f4e729fba36b54efb8d404b44e80489a18af5b33
tural <tsadigov@gmail.com>
2014-03-24 02:35:29 +0200

    upgrade was not working. Probably only drop command was being executed without create command.

--- bakkal/src/main/java/com/tsadigov/data/schema/Table.java
+++ bakkal/src/main/java/com/tsadigov/data/schema/Table.java
@@ -39,8 +39,10 @@ public abstract class Table {
      * @return String SQL statement
      */
     protected  String upgradeSql(int oldVersion, int newVersion){
-        return "DROP TABLE "+_name+" ; "+createSql();
+        return createSql();
     }
+
+
 
 
 
@@ -52,6 +54,11 @@ public abstract class Table {
     protected void onCreate(SQLiteDatabase db) {
         db.execSQL(createSql());
     }
+
+    protected String dropSql(){
+        return "DROP TABLE IF EXISTS "+_name+" ; ";
+    }
+
     /**
      * Upgrades the table that represents the associated entity. This will
      * typically be an ALTER TABLE statement.
@@ -62,6 +69,8 @@ public abstract class Table {
      */
     protected void onUpgrade(final SQLiteDatabase db, final int oldVersion,
                              final int newVersion) {
+
+        db.execSQL(dropSql());
         db.execSQL(upgradeSql(oldVersion, newVersion));
     }
 
